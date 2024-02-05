509a7cc646709047c496a87226c8564cbd6e435b
Sajib Bhattacharjee <86997775+Sajib-Bhattacharjee@users.noreply.github.com>
2024-02-06 00:56:28 +0600

    Update Product.js
    Ultimately I've solved the props problem.

--- Assignment-1(Product Listing App)/src/components/Product.js
+++ Assignment-1(Product Listing App)/src/components/Product.js
@@ -1,18 +1,17 @@
 /* eslint-disable */
 import React from 'react';
 
-const Product = (props) => {
-  const { id, title, price, description, category, image, rating } = props;
+const Product = ({ product }) => {
   return (
     <article className="product">
-      <img className="product__img" src={image} alt="Product Image" />
+      <img className="product__img" src={product.image} alt="Product Image" />
       <div className="product__details">
-        <h4 className="product__title">{title}</h4>
-        <p className="product__price">Price: $ {price}</p>
-        <p className="product__rating">Rating: {rating}</p>
-        <p className="product__rating">ID: {id}</p>
-        <p className="product__rating">Categroy: {category}</p>
-        <p className="product__desc">Description: {description}</p>
+        <h4 className="product__title">{product.title}</h4>
+        <p className="product__price">Price: $ {product.price}</p>
+        <p className="product__rating">Rating: {product.rating.rate}</p>
+        <p className="product__rating">ID: {product.id}</p>
+        <p className="product__rating">Categroy: {product.category}</p>
+        <p className="product__desc">Description: {product.description}</p>
         <button className="product__btn btn">Add to cart</button>
       </div>
     </article>
